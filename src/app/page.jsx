a5ab17b78ef2b5458ff6cdbbf0d0c89ed8371fef
ZiyadZK <kakangtea74@gmail.com>
2024-03-24 13:54:21 +0700

    Finishing Hero Page

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,8 +1,8 @@
 'use client'
 
-import { poppinsBold, poppinsLight, quicksand } from "@/libs/config/fonts";
+import { poppinsBold, poppinsLight, poppinsMedium, quicksand } from "@/libs/config/fonts";
 import { getAllUser } from "@/libs/database/models/userModel";
-import { faBars } from "@fortawesome/free-solid-svg-icons";
+import { faBars, faSignOut } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { useEffect, useState } from "react";
 
@@ -22,7 +22,8 @@ export default function Home() {
   }, [])
   return (
     <div className="w-full h-screen bg-zinc-950">
-      <nav className="fixed top-0 left-0 w-full backdrop-blur bg-zinc-950/10 border-b border-b-zinc-800 md:px-20 px-2 py-3 flex items-center justify-between">
+
+      <nav className="fixed z-50 top-0 left-0 w-full backdrop-blur bg-zinc-950/90 border-b border-b-zinc-800 md:px-20 px-5 py-3 flex items-center justify-between">
         <div className="flex items-center gap-2 md:gap-5">
           <img className="w-8 md:w-10 h-8 md:h-10" src="https://qzjbh7uie25fdc9m.public.blob.vercel-storage.com/logo-hima-AmwbkVJ8jHTvMmxHI9mSGvhcir0ErP.png" alt="" />
           <span className="text-zinc-700 md:text-2xl">/</span>
@@ -52,7 +53,45 @@ export default function Home() {
             </a>
           </div>
         </div>
+        <div className="flex items-center gap-5">
+          <p className={`${quicksand.className} text-zinc-500 hidden md:block`}>
+            Selamat Datang, <span className="text-transparent bg-clip-text bg-gradient-to-t from-zinc-300 to-white font-bold">Ziyad Jahizh K</span>
+          </p>
+          <button type="button" className="hover:scale-110 transition-all duration-300" title="Profile">
+            <img className="w-8 h-8 rounded-full object-cover" src="https://www.benarnews.org/indonesian/opini/opini-prabowo-ke-as-10152020224352.html/201015_ID_Prabowo_1000.jpg/@@images/image/social_media" alt="" />
+          </button>
+          <button type="button" className="w-8 h-8 bg-zinc-200/10 hover:bg-zinc-200 text-zinc-200 rounded flex items-center justify-center hover:text-zinc-800 transition-all duration-300">
+            <FontAwesomeIcon icon={faSignOut} className="w-4 h-4 text-inherit" />
+          </button>
+        </div>
       </nav>
+
+      <div className="w-full h-screen relative overflow-hidden">
+        <div className="absolute top-0 left-0 w-full h-screen flex items-center justify-center z-10 ">
+          <img className="w-full h-full object-cover" src="https://qzjbh7uie25fdc9m.public.blob.vercel-storage.com/foto-studio-hima-20232024" alt="" />
+        </div>
+        <div className="absolute top-0 left-0 w-full h-screen px-10 bg-gradient-to-t from-zinc-950 to-zinc-950/10 z-20 text-white">
+          <div className="w-full h-full flex items-center justify-center ">
+            <div className="flex flex-col items-center gap-1">
+              <p className={`${quicksand.className} text-zinc-400`}>an Official Website of</p>
+              <h1 className={`${poppinsBold.className} text-transparent bg-clip-text bg-gradient-to-t from-zinc-400 to-white text-3xl md:text-6xl text-center`}>
+                HIMA Informatika
+              </h1>
+              <h2 className={`${poppinsMedium.className} text-transparent bg-clip-text bg-gradient-to-t from-zinc-400 to-white text-xl md:text-3xl text-center tracking-tighter`}>
+                STMIK Bandung
+              </h2>
+              
+            </div>
+          </div>
+        </div>
+        
+
+      </div>
+      <div className="w-full h-screen relative overflow-hidden">
+        <div className="absolute top-0 left-0 w-full h-screen flex items-center justify-center px-10 py-40 md:px-40 md:py-10">
+          <div className="w-full h-full border border-zinc-600 border-dashed"></div>
+        </div>
+      </div>
     </div>
   );
 }
